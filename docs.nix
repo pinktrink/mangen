4e008408cda0029ac3929b85d0bffbb2d06db9df
Princess Chloe <lolbummer@gmail.com>
2023-09-29 02:36:39 -0700

    Sync push for testing.

--- docs.nix
+++ docs.nix
@@ -8,6 +8,18 @@ in {
       url = "https://firehol.org/fireqos-manual.pdf";
       sha256 = "1dvachsr6xkazhx1vcnvfiij524xgc6kv9a17a2n1119anari04c";
     };
+    "system/redhat-linux-7-admin-guide.pdf" = {
+      url = "https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/pdf/system_administrators_guide/red_hat_enterprise_linux-7-system_administrators_guide-en-us.pdf";
+      sha256 = "0kicxwqmb8z06yk75h5grcy31nk00l1crx3caxmwl0awfh9mxkvj";
+    };
+    "development/languages/haskell/learnyouahaskell.pdf" = {
+      url = "http://learnyouahaskell.com/learnyouahaskell.pdf";
+      sha256 = "1hzgmqmha4520snb0sj5j26zfbd875inh1c0id5ga2gvwm1vz1aw";
+    };
+    "development/languages/nix/nix-thesis.pdf" = {
+      url = "https://edolstra.github.io/pubs/nspfssd-lisa2004-final.pdf";
+      sha256 = "0ffrp64pcrr2f2m9lrmy7c07h5lw2y3c15qnf1r36prgpjqx3l90";
+    };
   };
 
   docs = with pkgs; {
@@ -55,6 +67,8 @@ in {
         usbutils
       ];
       coreutils.mans = [ coreutils-full ];
+      files.mans = [ rsync ];
+      rofi.mans = [ rofi ];
       systemd.mans = [ systemd ];
       zsh.mans = [ zsh ];
     };
--- flake.nix
+++ flake.nix
@@ -1,15 +1,30 @@
 {
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { nixpkgs, ... }: let
+  outputs = { self, nixpkgs, ... }: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
 
-    inherit (pkgs) symlinkJoin runCommand fetchurl;
+    inherit (pkgs) symlinkJoin runCommand fetchurl writeShellScript;
     inherit (pkgs.stdenv) mkDerivation;
     inherit (pkgs.lib.lists) map;
     inherit (pkgs.lib.strings) concatStringsSep replaceStrings;
     inherit (pkgs.lib.attrsets) mapAttrsRecursive foldlAttrs mapAttrs concatMapAttrs isAttrs;
+
+    d = x: builtins.trace x x;
+
+    formats = {
+      asciidoc = "adoc";
+      markdown = "md";
+      gfm = "gf.md";
+      latex = "tex";
+      epub = "epub";
+      docbook = "dbk";
+      mediawiki = "mwk";
+      rtf = "rtf";
+      rst = "rst";
+    };
 
     flattenMans = s: let
       unnest = p: s: concatMapAttrs (n: v: if (isAttrs v) then
@@ -32,7 +47,7 @@
     dlPaths = foldlAttrs (a: n: v: a + ''
       dlPaths[${n}]="${v}"
     '') "" dls;
-    builder = c: ''
+    builder = c: d ''
       declare -A buildPaths
       ${buildPaths}
       declare -A dlPaths
@@ -43,34 +58,33 @@
         mkdir -p "$out/$i"
         for j in $(find "''${buildPaths[$i]}/share/man" -not -type d); do
           f=$(basename "$j")
+          if [[ $(zcat "$j" | wc -l) -le 1 ]]; then
+            continue
+          fi
           if ! ${c}; then
             echo $j >> "$out/failed"
           fi
         done
       done
       for i in "''${!dlPaths[@]}"; do
+        mkdir -p "$out"/"$(dirname "$i")"
         echo cp "''${dlPaths[$i]}" "$out/$i"
         cp "''${dlPaths[$i]}" "$out/$i"
       done
-      find $out -type f -size -4c -delete
     '';
   in {
     packages.x86_64-linux = mapAttrs (n: v: runCommand "mangen-${n}" {
       nativeBuildInputs = with pkgs; [ coreutils pandoc gzip findutils wget ];
-    } (builder ''gunzip -c "$j" | pandoc -f man -t ${n} -o "$out/$i/''${f%.*}.${v}"'')) {
-      asciidoc = "adoc";
-      markdown = "md";
-      gfm = "gf.md";
-      latex = "tex";
-      epub = "epub";
-      docbook = "dbk";
-      mediawiki = "mwk";
-      rtf = "rtf";
-      rst = "rst";
-    } // {
+    } (builder ''gunzip -c "$j" | pandoc -f man -t ${n} -o "$out/$i/''${f%.*}.${v}"'')) formats // {
       pdf = runCommand "mangen-pdf" {
         nativeBuildInputs = with pkgs; [ coreutils man findutils ghostscript wget ];
       } (builder ''man -t "$j" | ps2pdf - "$out/$i/''${f%.*}.pdf"'');
     };
+    apps.x86_64-linux = mapAttrs (n: v: {
+      type = "app";
+      program = (writeShellScript "sync-mans-${n}" ''
+        ${pkgs.rsync}/bin/rsync -au --delete "${self.packages.x86_64-linux.${n}}/" "/home/chloe/etc/obsidian/docs"
+      '').outPath;
+    }) (formats // { pdf = "pdf"; });
   };
 }
